5dd57faf3c7f6d8214c3522ef3e47d65eba618e7
euntae-bae <euntae471@gmail.com>
2020-10-06 03:05:08 +0900

    Add lab2.cpp

---
+++ lab2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using std::cout;
+using std::endl;
+using std::string;
+
+int main(void)
+{
+	string str1 = "Hello! ";
+	string str2 = "lab2.cpp";
+
+	cout << str1 + str2 << endl;
+	return 0;
+}
